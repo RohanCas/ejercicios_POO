116bad539881b700d1c96f898ae588d7173f761b
RohanCas <rolando.castro2023@alu.uct.cl>
2023-09-04 10:42:13 -0400

    Parte 2 ejercicios c++

--- ejercicios3.cc
+++ ejercicios3.cc
@@ -1,2 +1,38 @@
-#ifndef CIRCULO-H
+#include <iostream>
+#include <cmath>
+
+class Circulo {
+private:
+    double radio;
+
+public:
+    Circulo(double r) {
+        radio = r;
+    }
+
+    double calcularArea() {
+        return M_PI * pow(radio, 2);
+    }
+
+    double calcularPerimetro() {
+        return 2 * M_PI * radio;
+    }
+
+    void cambiarRadio(double nuevoRadio) {
+        radio = nuevoRadio;
+    }
+};
+
+int main() {
+    Circulo miCirculo(5.0);
+
+    double area = miCirculo.calcularArea();
+    std::cout << "Área del círculo: " << area << std::endl;
+
+
+    double perimetro = miCirculo.calcularPerimetro();
+    std::cout << "Perímetro del círculo: " << perimetro << std::endl;
+
+    return 0;
+}
 
